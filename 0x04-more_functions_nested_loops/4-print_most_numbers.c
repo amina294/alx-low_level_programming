0764dbf578af8d6a9ebb27b934962f0a264cdcfa
amina294 <aminarufai294@yahoo.com>
2022-04-17 16:15:03 +0100

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,34 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
-void print_remaining_days(int month, int day, int year)
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
 {
-if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+int i;
+i = 0;
+while (i < 10)
 {
-if (month >= 3 && day >= 60)
+if (i != 2 && i != 4)
 {
-day++;
+_putchar (i + '0');
 }
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+i++;
 }
-else
-{
-if (month == 2 && day == 60)
-}
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
+_putchar ('\n');
 }
